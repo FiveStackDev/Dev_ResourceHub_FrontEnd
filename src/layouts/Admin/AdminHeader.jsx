00850dcc7b4f87cd14a8350cff3019e8f457bb0a
minulck <minulck@gmail.com>
2025-07-17 17:28:18 +0530

    refactor: Remove unused imports in AdminHeader component for cleaner code

--- src/layouts/Admin/AdminHeader.jsx
+++ src/layouts/Admin/AdminHeader.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import AppHeader from '../shared/AppHeader';
 import { getUnreadCount } from '../../utils/notificationApi';
 import { useUser, decodeToken } from '../../contexts/UserContext';
-import { getAuthHeader } from '../../utils/authHeader';
-import { BASE_URLS } from '../../services/api/config';
 
 const AdminHeader = () => {
 
