f2b7f112e0384eb4f01aefbe6224a4dd0f1ab7ff
Theekshana Udara <theekshana.uda@gmail.com>
2025-07-23 21:27:39 +0530

    feat: Update refetch interval in dashboard queries to improve data freshness

--- src/query/userDashboardQueries.js
+++ src/query/userDashboardQueries.js
@@ -58,6 +58,6 @@ export function useUserDashboardData() {
     },
     staleTime: 1000 * 60, // 1 minute
     retry: 2,
-    refetchInterval: 5000, // Refetch every 5 seconds
+    refetchInterval: 2500, // Refetch every 5 seconds
   });
 }
