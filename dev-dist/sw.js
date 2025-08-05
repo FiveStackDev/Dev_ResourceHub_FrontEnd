4c0aaf280a6b52d42a2673222618b625930b7cf4
Theekshana Udara <theekshana.uda@gmail.com>
2025-08-05 22:26:48 +0530

    fix: update revision for precached index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.j3ifcu6ua58"
+    "revision": "0.1km94g11hto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
